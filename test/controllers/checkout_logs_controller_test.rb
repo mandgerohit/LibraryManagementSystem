1a549da413cf290184aef43d56e29953f9deff1e
nikunj91 <shah_nikunj91@hotmail.com>
2015-09-25 19:08:24 -0400

    Created the Test cases

--- test/controllers/checkout_logs_controller_test.rb
+++ test/controllers/checkout_logs_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class CheckoutLogsControllerTest < ActionController::TestCase
+user=User.create(name: "pp",email: "pp@jj.com",password: "pass",password_confirmation: "pass",admin: "no",member: "yes")
   test "should get new" do
     get :new
     assert_response :success
   end
-
+  test "should fail if user not admin" do
+    
+    get :checkout_log_params
+    assert_response :fail
 end
+
+
